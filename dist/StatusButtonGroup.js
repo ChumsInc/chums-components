69c1c72859176e470e61baf393331c4e772a21bc
Steve Montgomery <steve@chums.com>
2022-06-21 08:22:15 -0600

    feat(StatusButtonGroup): add StatusButtonGroup component

---
+++ dist/StatusButtonGroup.js
@@ -0,0 +1,15 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import classNames from "classnames";
+const StatusButtonGroup = ({ checked, onChange }) => {
+    const btnEnabled = {
+        'btn-success': checked,
+        'btn-outline-success': !checked,
+    };
+    const btnDisabled = {
+        'btn-danger': !checked,
+        'btn-outline-danger': checked,
+    };
+    return (_jsxs("div", { className: "btn-group btn-group-sm", role: "group", children: [_jsx("button", { type: "button", onClick: () => onChange(true), className: classNames('btn btn-sm', btnEnabled), children: "Enabled" }), _jsx("button", { type: "button", onClick: () => onChange(false), className: classNames('btn btn-sm', btnDisabled), children: "Disabled" })] }));
+};
+export default StatusButtonGroup;
+//# sourceMappingURL=StatusButtonGroup.js.map
