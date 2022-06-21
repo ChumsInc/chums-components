f997158d43f28905bb4de6b97715ae12038d7621
Steve Montgomery <steve@chums.com>
2022-06-21 08:22:54 -0600

    feat(FormCheck): add id, disabled properties

--- dist/FormCheck.js
+++ dist/FormCheck.js
@@ -1,7 +1,9 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import classNames from 'classnames';
-const FormCheck = ({ label, checked, onClick, inline = false, className = {}, type = "checkbox" }) => {
-    return (_jsxs("div", { className: classNames("form-check", className, { "form-check-inline": inline }), children: [_jsx("input", { type: type, className: "form-check-input", checked: checked, onChange: onClick }), _jsx("label", { className: "form-check-label", onClick: onClick, children: label })] }));
+import { v4 as uuid4 } from 'uuid';
+const FormCheck = ({ id, label, checked, onClick, inline = false, className = {}, type = "checkbox", disabled }) => {
+    const inputId = id || uuid4();
+    return (_jsxs("div", { className: classNames("form-check", className, { "form-check-inline": inline }), children: [_jsx("input", { type: type, className: "form-check-input", id: inputId, checked: checked, disabled: disabled, onChange: onClick }), _jsx("label", { className: "form-check-label", htmlFor: inputId, children: label })] }));
 };
 export default FormCheck;
 //# sourceMappingURL=FormCheck.js.map
