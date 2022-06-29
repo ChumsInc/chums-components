a1a72610367e0063d604d49df3bee688465d7b97
Steve Montgomery <steve@chums.com>
2022-06-29 07:46:21 -0600

    chore(FormCheck): run build to commit changes made in FormCheck.tsx file

--- dist/FormCheck.js
+++ dist/FormCheck.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import classNames from 'classnames';
 import { v4 as uuid4 } from 'uuid';
-const FormCheck = ({ id, label, checked, onChange, inline = false, className = {}, type = "checkbox", ...props }) => {
+const FormCheck = ({ id, label, checked, onChange, inline = false, className, type = "checkbox", ...props }) => {
     const inputId = id || ('form-check-' + uuid4());
     return (_jsxs("div", { className: classNames("form-check", className, { "form-check-inline": inline }), children: [_jsx("input", { type: type, className: "form-check-input", id: inputId, checked: checked, onChange: onChange, ...props }), _jsx("label", { className: "form-check-label", htmlFor: inputId, children: label })] }));
 };
