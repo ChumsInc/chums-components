e42611e1714c0caab81261058ff77925d68f2985
Steve Montgomery <steve@chums.com>
2022-07-01 09:24:52 -0600

    fix(FormCheck): Change parent props to React.ComponentProps

--- dist/FormCheck.d.ts
+++ dist/FormCheck.d.ts
@@ -1,10 +1,10 @@
 import React from 'react';
-export interface FormCheckProps extends React.HTMLAttributes<HTMLInputElement> {
+export interface FormCheckProps extends React.ComponentProps<"input"> {
+    type: 'radio' | 'checkbox';
     id?: string;
     label: string;
     checked: boolean;
     inline?: boolean;
-    type: 'radio' | 'checkbox';
 }
 declare const FormCheck: React.FC<FormCheckProps>;
 export default FormCheck;
--- src/FormCheck.tsx
+++ src/FormCheck.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import classNames from 'classnames';
 import {v4 as uuid4} from 'uuid';
 
-export interface FormCheckProps extends React.HTMLAttributes<HTMLInputElement> {
+export interface FormCheckProps extends React.ComponentProps<"input"> {
+    type: 'radio' | 'checkbox',
     id?: string,
     label: string,
     checked: boolean,
     inline?: boolean,
-    type: 'radio' | 'checkbox',
 }
 
 const FormCheck: React.FC<FormCheckProps> = ({
+                                                 type = "checkbox",
                                                  id,
                                                  label,
                                                  checked,
                                                  onChange,
                                                  inline = false,
                                                  className,
-                                                 type = "checkbox",
+                                                 disabled,
                                                  ...props
                                              }) => {
     const inputId = id || ('form-check-' + uuid4());
@@ -25,7 +26,7 @@ const FormCheck: React.FC<FormCheckProps> = ({
     return (
         <div className={classNames("form-check", className, {"form-check-inline": inline})}>
             <input type={type} className="form-check-input"
-                   id={inputId} checked={checked}
+                   id={inputId} checked={checked} disabled={disabled}
                    onChange={onChange} {...props}/>
             <label className="form-check-label" htmlFor={inputId}>
                 {label}
